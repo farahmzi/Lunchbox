2afd1bf4afabcd99a0b8898774e7dac9fa09d4b9
Farah <farah.dev@icloud.com>
2025-09-25 14:36:03 +0300

    Initial Commit

---
+++ Lunchbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lunchbox
+//
+//  Created by Farah Almozaini on 03/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lunchbox/LunchboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchboxApp.swift
+//  Lunchbox
+//
+//  Created by Farah Almozaini on 03/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LunchboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
