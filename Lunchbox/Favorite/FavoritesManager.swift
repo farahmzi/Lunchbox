dc0f0e7ca830e6e35a6c8bdd4916772a85eaa6de
Farah <farah.dev@icloud.com>
2025-10-06 21:59:52 +0300

    Add files

---
+++ Lunchbox/Favorite/FavoritesManager.swift
@@ -0,0 +1,18 @@
+////  FavoritesManager.swift
+//
+//import SwiftUI
+//
+//class FavoritesManager: ObservableObject {
+//    @Published var favorites: [Recipe] = []
+//
+//    func addToFavorites(_ recipe: Recipe) {
+//        if !favorites.contains(recipe) {
+//            favorites.append(recipe)
+//        }
+//    }
+//
+//    func removeFromFavorites(_ recipe: Recipe) {
+//        favorites.removeAll { $0 == recipe }
+//    }
+//}
+
